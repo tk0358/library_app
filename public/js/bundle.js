3e2f60b36ad5982febe302e867d783ac9f6b9153
tk0358 <tk0358a@yahoo.co.jp>
2021-01-25 22:45:28 +0900

    changed bundle

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8809,7 +8809,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56833" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59466" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
